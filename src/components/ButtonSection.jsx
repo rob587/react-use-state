96271ccd2906499b418f73c680b1327f9bd6ad10
rob587 <robertocamm02@yahoo.com>
2025-08-28 23:28:05 +0200

    finished

--- src/components/ButtonSection.jsx
+++ src/components/ButtonSection.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Description from './Description'
 
 const ButtonSection = () => {
-  
+  const [id_, setId] = useState(null)
   return (
     <div>
         <div className="container">
             <div className="row">
-                <div className="col-12">
-                    <div className="btn-box d-flex      justify-content-center py-5  ">
-                    <button onClick={()=>Description(1)} className="btn btn-primary mx-3">HTML</button>
-                    <button onClick={()=>Description(2)} className="btn btn-primary mx-3">CSS</button>
-                    <button onClick={()=>Description(3)} className="btn btn-primary mx-3">Javascript</button>
-                    <button onClick={()=>Description(4)} className="btn btn-primary mx-3">Node.js</button>
-                    <button onClick={()=>Description(5)}  className="btn btn-primary mx-3">Express</button>
-                    <button onClick={()=>Description(6)} className="btn btn-primary mx-3">React.js</button>
-                
+              <div className="col"><h1>Learn Web Develop</h1>
+              </div>
+                <div className="col-12 ">
+                    <div className="btn-box  justify-content-center py-5   ">
+                    <button onClick={()=>setId(1)} className="btn btn-primary mx-3">HTML</button>
+                    <button onClick={()=>setId(2)} className="btn btn-primary mx-3">CSS</button>
+                    <button onClick={()=>setId(3)} className="btn btn-primary mx-3">Javascript</button>
+                    <button onClick={()=>setId(4)} className="btn btn-primary mx-3">Node.js</button>
+                    <button onClick={()=>setId(5)}  className="btn btn-primary mx-3">Express</button>
+                    <button onClick={()=>setId(6)} className="btn btn-primary mx-3">React.js</button>
+                    {
+                      id_ && <Description id={id_} />
+                    }
                     </div>
 
                 </div>
--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import languages from './languages'
 
-const Description = () => {
+const Description = ({id}) => {
+        const language = languages.find(lang => lang.id === id)
+          if (!language) return null
   return (
         <div className="container">
             <div className="row">
                 <div className="col">
-                    <div class="card w-25  ">
+                    <div class="card w-25 ">
                         <div class="card-body">
-                            <h5 class="card-title" >{languages[3]["title"]}</h5>
-                            <p class="card-text">{languages[3]["description"]}</p>
+                            <h5 class="card-title" >{language.title}</h5>
+                            <p class="card-text">{language.description}</p>
                            
                         </div>
                      </div>
